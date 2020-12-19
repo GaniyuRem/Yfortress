a8e8780dcebcaab3c9f328317d617f25da24ca1f
atua2 <chimezie.emeka@gmail.com>
2020-12-19 01:15:23 +0000

    project 10 updated

--- Yearn_project/settings.py
+++ Yearn_project/settings.py
@@ -118,4 +118,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 print("hello", STATICFILES_DIRS)
-django_heroku.settings(local())
+django_heroku.settings(locals())
