1521a7d9f8e83843e16c6f83c1ac121515d3bdaa
Ganiyu <odunaiyakola@gmail.com>
2020-12-30 22:57:06 +0000

    setting updated 7

--- Yearn_project/settings.py
+++ Yearn_project/settings.py
@@ -71,9 +71,9 @@ WSGI_APPLICATION = 'Yearn_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yearn_project',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PWD'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER_NAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
     }
 }
 
