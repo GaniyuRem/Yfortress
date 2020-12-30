3661bf4a4107613ed78c060b0867424d1dc6684d
Ganiyu <odunaiyakola@gmail.com>
2020-12-30 20:06:59 +0000

    heroku requirmens file

--- Yearn_project/settings.py
+++ Yearn_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+django_on_heroku.settings(locals())
